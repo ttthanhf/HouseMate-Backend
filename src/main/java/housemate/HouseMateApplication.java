9ed881c13c907b7c227a842ed41e80091109236d
ThanhF <trthanh179@gmail.com>
2023-09-21 11:24:12 +0700

    Add file each package

--- src/main/java/housemate/HouseMateApplication.java
+++ src/main/java/housemate/HouseMateApplication.java
@@ -1,5 +1,3 @@
-package housemate.application;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
