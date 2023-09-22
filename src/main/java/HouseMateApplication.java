e5730c9b7936b47232d87e2176ac73b9641348d2
ThanhF <trthanh179@gmail.com>
2023-09-22 10:07:40 +0700

    Setup MainClass path and add example docker

--- src/main/java/HouseMateApplication.java
+++ src/main/java/HouseMateApplication.java
@@ -1,8 +1,9 @@
+
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 /**
  *
  * @author ThanhF
@@ -15,5 +16,5 @@ public class HouseMateApplication {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
--- src/test/java/housemate/backend/HousemateApplicationTests.java
+++ src/test/java/housemate/backend/HousemateApplicationTests.java
@@ -1,13 +1,13 @@
-package housemate.backend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HousemateApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package housemate.backend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class HousemateApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
