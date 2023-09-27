e9c49a0ada6ff0e991d45cea3a0ce210c947b5ae
Tran Tan Thanh <88179750+ttthanhf@users.noreply.github.com>
2023-09-27 15:23:44 +0700

    .

---
+++ src/main/java/housemate/controllers/LoginController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package housemate.controllers;
+
+import housemate.services.LoginService;
+import java.util.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ *
+ * @author ThanhF
+ */
+@RestController
+@RequestMapping
+@CrossOrigin
+public class LoginController {
+
+    @Autowired
+    LoginService loginService;
+
+    @GetMapping("/callback/google/redirect")
+    public ResponseEntity<String> loginSuccessWithGoogle(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+        Map<String, Object> user = oAuth2AuthenticationToken.getPrincipal().getAttributes();
+        return loginService.loginWithGoogle(user);
+    }
+}
