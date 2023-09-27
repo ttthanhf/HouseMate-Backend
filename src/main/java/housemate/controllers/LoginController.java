db746e5c9dce6551e17ff3a4a0b571f5ce023e7c
ThanhF <trthanh179@gmail.com>
2023-09-27 14:40:39 +0700

    Remove all unuse import. variable construstor in LoginController

--- src/main/java/housemate/controllers/LoginController.java
+++ src/main/java/housemate/controllers/LoginController.java
@@ -4,15 +4,12 @@
  */
 package housemate.controllers;
 
-import housemate.entities.UserAccount;
 import housemate.services.LoginService;
 import java.util.*;
 import org.springframework.beans.factory.annotation.*;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.*;
-import java.net.URI;
 
 /**
  *
@@ -23,14 +20,8 @@ import java.net.URI;
 @CrossOrigin
 public class LoginController {
 
-    private final String redirectUri;
-
     @Autowired
     LoginService loginService;
-
-    public LoginController(@Value("${application.setting.google.redirect-uri}") String redirectUri) {
-        this.redirectUri = redirectUri;
-    }
 
     @GetMapping("/callback/google/redirect")
     public ResponseEntity<String> loginSuccessWithGoogle(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
