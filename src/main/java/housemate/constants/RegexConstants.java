e87e5d3a8b6e2f9f3d8bc71dcba89be1a398f0f5
hdang09 <dangtranhai628@gmail.com>
2023-09-28 05:39:03 +0100

    REFACTOR - Rename RegexConstants file

--- src/main/java/housemate/constants/RegexConstants.java
+++ src/main/java/housemate/constants/RegexConstants.java
@@ -8,7 +8,7 @@ package housemate.constants;
  *
  * @author Admin
  */
-public interface Regex {
-    String PASSWORD = "^(?=.[0-9])(?=.[a-z])(?=.*[A-Z]).{8,16}$";
-    String PHONE_NUMBER = "^(0|\\+?84)(3|5|7|8|9)[0-9]{8}$";
+public interface RegexConstants {
+    String PASSWORD_REGEX = "^(?=.[0-9])(?=.[a-z])(?=.*[A-Z]).{8,16}$";
+    String PHONE_NUMBER_REGEX = "^(0|\\+?84)(3|5|7|8|9)[0-9]{8}$";
 }
