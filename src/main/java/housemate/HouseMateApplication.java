90953db661189895d96847decbe1899a53df9f53
hdang09 <dangtranhai628@gmail.com>
2023-09-29 08:04:11 +0100

    UPDATE - Add swagger definition and fix password regex error

--- src/main/java/housemate/HouseMateApplication.java
+++ src/main/java/housemate/HouseMateApplication.java
@@ -1,5 +1,7 @@
 package housemate;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author ThanhF
  */
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "HouseMate API",
+                version = "1.0.0",
+                description = "An API for the software system provides single service and package service for student apartments."
+        )
+)
 public class HouseMateApplication {
 
     /**
