62a5f7b604db881863006395f03bb40ccd485803
ThanhF <trthanh179@gmail.com>
2023-10-03 12:19:48 +0700

    Add Tag for CartController and add Operation for Mapping

--- src/main/java/housemate/controllers/CartController.java
+++ src/main/java/housemate/controllers/CartController.java
@@ -7,7 +7,9 @@ package housemate.controllers;
 import housemate.entities.Cart;
 import housemate.models.CartAddDTO;
 import housemate.services.CartService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import java.util.List;
@@ -30,21 +32,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cart")
 @CrossOrigin
 @SecurityRequirement(name = "bearerAuth")
+@Tag(name = "Cart")
 public class CartController {
 
     @Autowired
     CartService cartService;
 
+    @Operation(summary = "Get user cart by userId in token")
     @GetMapping("/")
     public ResponseEntity<List<Cart>> getCart(HttpServletRequest request) {
         return cartService.getCart(request);
     }
 
+    @Operation(summary = "Add to cart. When cart already exist -> update quantity only")
     @PostMapping("/")
     public ResponseEntity<String> addCart(HttpServletRequest request, @RequestBody CartAddDTO cartAdd) {
         return cartService.addToCart(request, cartAdd);
     }
 
+    @Operation(summary = "Detele cart when cart exist")
     @DeleteMapping("/{cartId}")
     public ResponseEntity<String> removeCart(HttpServletRequest request, @Valid @PathVariable int cartId) {
         return cartService.removeCart(request, cartId);
