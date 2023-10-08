f176ac3a5f84bd3e77cbfe803dbf8cc23f8061f9
ThanhF <trthanh179@gmail.com>
2023-10-08 23:25:00 +0700

    Add stable payment with vnpay

--- src/main/java/housemate/controllers/PaymentController.java
+++ src/main/java/housemate/controllers/PaymentController.java
@@ -4,10 +4,40 @@
  */
 package housemate.controllers;
 
+import housemate.services.PaymentService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author ThanhF
  */
+@RestController
+@RequestMapping("/payment")
+@CrossOrigin
+@Tag(name = "Payment")
+@SecurityRequirement(name = "bearerAuth")
 public class PaymentController {
-    
+
+    @Autowired
+    PaymentService paymentService;
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createPayment(HttpServletRequest request) throws UnsupportedEncodingException {
+        return paymentService.createVNPayPayment(request);
+    }
+
+    @GetMapping("/check")
+    public void createPaymentasdsad(HttpServletRequest request) {
+        return;
+    }
 }
---
+++ src/main/java/housemate/services/PaymentService.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package housemate.services;
+
+import housemate.configs.VNPayConfig;
+import housemate.repositories.CartRepository;
+import housemate.utils.AuthorizationUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+import java.io.UnsupportedEncodingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author ThanhF
+ */
+@Service
+public class PaymentService {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private AuthorizationUtil authorizationUtil;
+
+    private String vnp_Version = "2.1.0";
+    private String vnp_Command = "pay";
+    private String orderType = "other";
+    private String language = "vn";
+    private String bankCode = "NCB";
+
+    private String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+    private String vnp_ReturnUrl = "http://localhost:8080/vnpay_jsp/vnpay_return.jsp";
+    private String vnp_TmnCode = "ME9TNTA9";
+    private String secretKey = "XHHCNMOVFMBAKNPHICPZWAPAJBPUQAKA";
+
+    private String vnp_IpAddr = "127.0.0.1";
+
+    public ResponseEntity<String> createVNPayPayment(HttpServletRequest request) throws UnsupportedEncodingException {
+
+        long pricePerService = 1000000 * 100;
+        int totalService = cartRepository.getTotalCart(authorizationUtil.getUserIdFromAuthorizationHeader(request));
+
+        long amount = pricePerService * totalService;
+
+        String vnp_TxnRef = VNPayConfig.getRandomNumber(8);
+
+        Map<String, String> vnp_Params = new HashMap<>();
+        vnp_Params.put("vnp_Version", vnp_Version);
+        vnp_Params.put("vnp_Command", vnp_Command);
+        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+        vnp_Params.put("vnp_CurrCode", "VND");
+
+        if (bankCode != null && !bankCode.isEmpty()) {
+            vnp_Params.put("vnp_BankCode", bankCode);
+        }
+
+        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
+        vnp_Params.put("vnp_OrderType", orderType);
+        vnp_Params.put("vnp_Locale", language);
+        vnp_Params.put("vnp_ReturnUrl", vnp_ReturnUrl);
+        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        String vnp_CreateDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+
+        cld.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+        List fieldNames = new ArrayList(vnp_Params.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder hashData = new StringBuilder();
+        StringBuilder query = new StringBuilder();
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) vnp_Params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                hashData.append(fieldName);
+                hashData.append('=');
+                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                query.append('=');
+                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                if (itr.hasNext()) {
+                    query.append('&');
+                    hashData.append('&');
+                }
+            }
+        }
+        String queryUrl = query.toString();
+        String vnp_SecureHash = VNPayConfig.hmacSHA512(secretKey, hashData.toString());
+        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+        String paymentUrl = vnp_PayUrl + "?" + queryUrl;
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(paymentUrl);
+    }
+}
