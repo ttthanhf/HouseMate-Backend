bfa94606a97b3f82ffd94f4bca0f130d3eff6dad
Hoang Anh Nguyen <nguyenhoanganhgoahead@gmai.com>
2023-10-10 19:25:31 +0700

    update exception and reformat service controller

---
+++ src/main/java/housemate/exceptions/ApiException.java
@@ -0,0 +1,19 @@
+package housemate.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiException extends RuntimeException {
+	private HttpStatus code;
+	private String messgae;
+	
+	public ApiException(HttpStatus code, String messgae) {
+		super();
+		this.code = code;
+		this.messgae = messgae;
+	}
+}
