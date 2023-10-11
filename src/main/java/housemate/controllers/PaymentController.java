16d8de31baf06cdda360053c7750cfe35401ccbe
ThanhF <trthanh179@gmail.com>
2023-10-11 15:03:32 +0700

    Add - Add neccessary utils for vnpay and add controller check payment

--- src/main/java/housemate/controllers/PaymentController.java
+++ src/main/java/housemate/controllers/PaymentController.java
@@ -8,13 +8,14 @@ import housemate.services.PaymentService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -31,13 +32,13 @@ public class PaymentController {
     @Autowired
     PaymentService paymentService;
 
-    @PostMapping("/create")
+    @GetMapping("/create")
     public ResponseEntity<String> createPayment(HttpServletRequest request) throws UnsupportedEncodingException {
         return paymentService.createVNPayPayment(request);
     }
 
     @GetMapping("/check")
-    public void checkPayment(HttpServletRequest request) {
-        return;
+    public ResponseEntity<Object> checkPayment(@RequestParam String vnp_TxnRef, @RequestParam String vnp_TransactionNo, @RequestParam String vnp_PayDate) throws IOException {
+        return paymentService.checkVNPayPayment(vnp_TxnRef, vnp_TransactionNo, vnp_PayDate);
     }
 }
