030e62db0550b304c0e02edae43c4f6716cb85d0
ThanhF <trthanh179@gmail.com>
2023-10-14 10:24:17 +0700

    Add - add validation for UserInfoOrderDTO

--- src/main/java/housemate/models/UserInfoOrderDTO.java
+++ src/main/java/housemate/models/UserInfoOrderDTO.java
@@ -4,6 +4,11 @@
  */
 package housemate.models;
 
+import housemate.constants.RegexConstants;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 /**
@@ -13,9 +18,20 @@ import lombok.Data;
 @Data
 public class UserInfoOrderDTO {
 
+    @Size(min = 2, max = 200, message = "address must be between 2 and 200 characters")
+    @Schema(example = "Example", description = "Address")
     private String address;
+
+    @Pattern(regexp = RegexConstants.PHONE_NUMBER_REGEX, message = "Invalid phone number format")
+    @Schema(example = "0866123456", description = "Phone number")
     private String phone;
+
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    @Schema(example = "Example", description = "Full name")
     private String fullName;
+
+    @Email(message = "Please enter a valid email")
+    @Schema(example = "example@gmail.com", description = "Email")
     private String email;
 
 }
