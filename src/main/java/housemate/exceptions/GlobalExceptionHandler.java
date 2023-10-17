67fc894107ef5a6228921409920def636b34f889
Hoang Anh Nguyen <nguyenhoanganhgoahead@gmai.com>
2023-10-17 16:55:40 +0700

    reroute feedback

--- src/main/java/housemate/exceptions/GlobalExceptionHandler.java
+++ src/main/java/housemate/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package housemate.exceptions;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
@@ -19,30 +20,29 @@ import org.springframework.http.HttpStatus;
 @RestController
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(BindException.class)
-    public ResponseEntity<List<String>> handleValidationException(BindException ex) {
-        List<String> errors = new ArrayList<>();
+	@ExceptionHandler(BindException.class)
+	public ResponseEntity<List<String>> handleValidationException(BindException ex) {
+		List<String> errors = new ArrayList<>();
 
-        for (FieldError error : ex.getFieldErrors()) {
-            errors.add(error.getDefaultMessage());
-        }
-        return ResponseEntity.badRequest().body(errors);
-    }
-    
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
-    }  
-    
+		for (FieldError error : ex.getFieldErrors()) {
+			errors.add(error.getDefaultMessage());
+		}
+		return ResponseEntity.badRequest().body(errors);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleException(Exception ex) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+	}
+
 	@ExceptionHandler(HttpMessageNotReadableException.class)
 	public ResponseEntity<String> handleConverterException(Exception ex) {
 		String mess = ex.getMessage();
 		if (mess.contains("not one of the values accepted for Enum class")) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body( "Error enum for " +  mess.substring(mess.indexOf("Enum class: [")));
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body("Error enum for " + mess.substring(mess.indexOf("Enum class: [")));
 		}
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Convert Json To Object Faild. Some Error");
-	} 
+	}
 
-    }
-
-
+}
