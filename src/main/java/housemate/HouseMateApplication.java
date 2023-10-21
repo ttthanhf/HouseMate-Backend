68d7063ac183b2195d8fa6de80a42e603b48cb7b
hdang09 <dangtranhai628@gmail.com>
2023-10-21 15:36:00 +0700

    ADD - Add out of cycle range validation

--- src/main/java/housemate/HouseMateApplication.java
+++ src/main/java/housemate/HouseMateApplication.java
@@ -5,11 +5,6 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.format.Formatter;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 /**
  *
@@ -35,21 +30,5 @@ public class HouseMateApplication {
     public static void main(String[] args) {
         SpringApplication.run(HouseMateApplication.class, args);
     }
-
-    // Uncomment this code if datetime bug occur
-//    @Bean
-//    public Formatter<LocalDate> localDateFormatter() {
-//        return new Formatter<>() {
-//            @Override
-//            public LocalDate parse(String text, java.util.Locale locale) {
-//                return LocalDate.parse(text, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-//            }
-//
-//            @Override
-//            public String print(LocalDate object, java.util.Locale locale) {
-//                return DateTimeFormatter.ofPattern("dd/MM/yyyy").format(object);
-//            }
-//        };
-//    }
 
 }
