ec07a5f611c321eb4f2340ee83d96cb5a39f2abd
Hoang Anh Nguyen <nguyenhoanganhgoahead@gmai.com>
2023-10-22 15:34:54 +0700

    remove unused import

--- src/main/java/housemate/models/DeliveryScheduleDTO.java
+++ src/main/java/housemate/models/DeliveryScheduleDTO.java
@@ -8,8 +8,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import housemate.constants.Cycle;
 import jakarta.validation.constraints.*;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 
