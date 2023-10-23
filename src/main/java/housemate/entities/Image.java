0493fa3d3b2e5034d7b7fe8c05766fecb75a7829
ThanhF <trthanh179@gmail.com>
2023-10-23 12:33:56 +0700

    ADD - Add enum for upload and change format url image

--- src/main/java/housemate/entities/Image.java
+++ src/main/java/housemate/entities/Image.java
@@ -1,7 +1,10 @@
 package housemate.entities;
 
+import housemate.constants.ImageType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -35,6 +38,7 @@ public class Image {
     @Column(name = "entity_id")
     private int entityId;
 
-    @Column(name = "is_task")
-    private boolean isTask;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "image_type")
+    private ImageType imageType;
 }
