cea4ea0a43b75d1c53ec73624ff0206abb6a799e
ThanhF <trthanh179@gmail.com>
2023-10-25 15:02:45 +0700

    ADD - Add more uuid for url image generate

--- src/main/java/housemate/services/UploadService.java
+++ src/main/java/housemate/services/UploadService.java
@@ -20,6 +20,7 @@ import housemate.entities.Image;
 import housemate.models.UploadDTO;
 import housemate.repositories.ImageRepository;
 import housemate.utils.AuthorizationUtil;
+import housemate.utils.RandomUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -40,21 +41,21 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Service
 public class UploadService {
-
+    
     private final AmazonS3 s3client;
-
+    
     @Value("${s3.bucket}")
     private String bucket;
-
+    
     @Value("${application.domainCDN}")
     private String domainCDN;
-
+    
     @Autowired
     private ImageRepository imageRepository;
-
+    
     @Autowired
     private AuthorizationUtil authorizationUtil;
-
+    
     @Autowired
     public UploadService(
             @Value("${s3.endpoint}") String endpoint,
@@ -63,45 +64,45 @@ public class UploadService {
             @Value("${s3.secret-key}") String secretKey,
             @Value("${s3.bucket}") String bucket
     ) {
-
+        
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
         s3client = AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .withEndpointConfiguration(new EndpointConfiguration(endpoint, region))
                 .build();
     }
-
+    
     @Async
     public ResponseEntity<String> uploadImage(HttpServletRequest request, MultipartFile[] files, UploadDTO uploadDTO) {
-
+        
         if (uploadDTO.getEntityId() == 0 || uploadDTO.getImageType().equals(null) || uploadDTO.getImageType().equals("")) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing entity id or image type");
         }
-
+        
         if (files.length == 0) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Select at least one file to upload");
         }
-
+        
         for (MultipartFile file : files) {
             if (!file.getContentType().startsWith("image/")) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only image support");
             }
-
+            
             if (file.getSize() > 5 * 1024 * 1024) { // 5MB
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Image size must not exceed 5MB");
             }
-
+            
             long currentTimeStamp = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
             String extensionImage = ".webp";
-            String imageName = uploadDTO.getEntityId() + "." + uploadDTO.getImageType() + "." + currentTimeStamp + extensionImage;
-
+            String imageName = uploadDTO.getEntityId() + "." + uploadDTO.getImageType() + "." + currentTimeStamp + "." + RandomUtil.getRandomNumber(3) + extensionImage;
+            
             InitiateMultipartUploadRequest initRequest = new InitiateMultipartUploadRequest(bucket, imageName);
             InitiateMultipartUploadResult initResult = s3client.initiateMultipartUpload(initRequest);
-
+            
             String uploadId = initResult.getUploadId();
-
+            
             List<PartETag> partETags = new ArrayList<>();
-
+            
             long partSize = 2 * 1024 * 1024; // 2MB
             long fileSize = file.getSize();
             long totalParts = (long) Math.ceil((double) fileSize / partSize);
@@ -109,7 +110,7 @@ public class UploadService {
                 for (int partNumber = 1; partNumber <= totalParts; partNumber++) {
                     long start = (partNumber - 1) * partSize;
                     long end = Math.min(start + partSize, fileSize);
-
+                    
                     UploadPartRequest uploadPartRequest = new UploadPartRequest()
                             .withBucketName(bucket)
                             .withKey(imageName)
@@ -117,36 +118,36 @@ public class UploadService {
                             .withPartNumber(partNumber)
                             .withPartSize(end - start)
                             .withInputStream(file.getInputStream());
-
+                    
                     UploadPartResult uploadPartResult = s3client.uploadPart(uploadPartRequest);
                     partETags.add(uploadPartResult.getPartETag());
                 }
             } catch (IOException e) {
                 return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Can not read file !");
             }
-
+            
             CompleteMultipartUploadRequest completeRequest = new CompleteMultipartUploadRequest()
                     .withBucketName(bucket)
                     .withKey(imageName)
                     .withUploadId(uploadId)
                     .withPartETags(partETags);
-
+            
             s3client.completeMultipartUpload(completeRequest);
-
+            
             Image image = new Image();
             image.setImageUrl(domainCDN + imageName);
             image.setEntityId(uploadDTO.getEntityId());
             image.setImageType(uploadDTO.getImageType());
             image.setUserId(authorizationUtil.getUserIdFromAuthorizationHeader(request));
             imageRepository.save(image);
-
+            
         }
-
+        
         return ResponseEntity.status(HttpStatus.OK).body("Upload success");
     }
-
+    
     public ResponseEntity<String> removeImage(HttpServletRequest request, int entityId) {
-
+        
         String userRole = authorizationUtil.getRoleFromAuthorizationHeader(request);
         if (!userRole.equals(Role.ADMIN.toString())) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Only admin role");
@@ -155,7 +156,7 @@ public class UploadService {
         if (image == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Image not found");
         }
-
+        
         imageRepository.deleteById(entityId);
         return ResponseEntity.status(HttpStatus.OK).body("Deleted !");
     }
