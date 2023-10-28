c5b9bd2a1c8d7170b98f1675d19887a20af1b12e
hdang09 <dangtranhai628@gmail.com>
2023-10-29 01:22:35 +0700

    REMOVE - Remove hard code comment and remove update DTO

--- src/main/java/housemate/controllers/ScheduleController.java
+++ src/main/java/housemate/controllers/ScheduleController.java
@@ -53,42 +53,10 @@ public class ScheduleController {
         return service.getAllPurchased(request);
     }
 
-//    @Operation(summary = "Create schedule for Hourly Service")
-//    @PostMapping("/create/hourly")
-//    public ResponseEntity<String> createHourlySchedule(
-//            HttpServletRequest request, @Valid @RequestBody HourlyScheduleDTO hourlyScheduleDTO
-//    ) {
-//        return service.createHourlySchedule(request, hourlyScheduleDTO);
-//    }
-//
-//    @Operation(summary = "Create schedule for Return Service")
-//    @PostMapping("/create/return")
-//    public ResponseEntity<String> createReturnSchedule(
-//            HttpServletRequest request, @Valid @RequestBody ReturnScheduleDTO returnScheduleDTO
-//    ) {
-//        return service.createReturnSchedule(request, returnScheduleDTO);
-//    }
-//
-//    @Operation(summary = "Create schedule for Delivery Service")
-//    @PostMapping("/create/delivery")
-//    public ResponseEntity<String> createDeliverySchedule(
-//            HttpServletRequest request, @Valid @RequestBody DeliveryScheduleDTO deliveryScheduleDTO
-//    ) {
-//        return service.createDeliverySchedule(request, deliveryScheduleDTO);
-//    }
-
     @Operation(summary = "Create schedule")
     @PostMapping("/create")
     public ResponseEntity<String> createSchedule(HttpServletRequest request, @Valid @RequestBody ScheduleDTO scheduleDTO) {
         return service.createSchedule(request, scheduleDTO);
-    }
-
-    @Operation(summary = "Update schedule")
-    @PutMapping("/update/{scheduleId}")
-    public ResponseEntity<String> updateHourlySchedule(
-            HttpServletRequest request, @Valid @RequestBody ScheduleUpdateDTO scheduleUpdateDTO, @PathVariable int scheduleId
-    ) {
-        return service.updateSchedule(request, scheduleUpdateDTO, scheduleId);
     }
 
 }
--- src/main/java/housemate/mappers/ScheduleMapper.java
+++ src/main/java/housemate/mappers/ScheduleMapper.java
@@ -7,7 +7,6 @@ import housemate.models.ScheduleDTO;
 import housemate.responses.EventRes;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Component
@@ -26,66 +25,8 @@ public class ScheduleMapper {
         schedule.setStatus(ScheduleStatus.PROCESSING);
         schedule.setUserUsageId(schedule.getUserUsageId());
 
-        return null;
+        return schedule;
     }
-
-//    Uncomment these code if hard code working
-//    public Schedule mapToEntity(HourlyScheduleDTO hourlyScheduleDTO) {
-//        Schedule schedule = new Schedule();
-//        LocalDateTime startDate = hourlyScheduleDTO.getDate().atTime(hourlyScheduleDTO.getTimeRanges().get(0));
-//        LocalDateTime endDate = hourlyScheduleDTO.getDate().atTime(hourlyScheduleDTO.getTimeRanges().get(1));
-//        int quantityRetrieve = (int) Duration.between(startDate, endDate).toHours();
-//        String note = hourlyScheduleDTO.getNote();
-//
-//        schedule.setServiceId(hourlyScheduleDTO.getServiceId());
-//        schedule.setServiceTypeId(hourlyScheduleDTO.getTypeId());
-//        schedule.setQuantityRetrieve(quantityRetrieve);
-//        schedule.setStartDate(startDate);
-//        schedule.setEndDate(endDate);
-//        schedule.setNote(note == null ? "" : note.trim());
-//        schedule.setCycle(hourlyScheduleDTO.getCycle());
-//        schedule.setStatus(ScheduleStatus.PROCESSING);
-//        schedule.setUserUsageId(hourlyScheduleDTO.getUserUsageId());
-//
-//        return schedule;
-//    }
-//
-//    public Schedule mapToEntity(ReturnScheduleDTO returnScheduleDTO) {
-//        Schedule schedule = new Schedule();
-//        LocalDateTime pickupDateTime = returnScheduleDTO.getPickUpDate().atTime(returnScheduleDTO.getTime());
-//        LocalDateTime receivedDateTime = returnScheduleDTO.getReceiveDate().atTime(returnScheduleDTO.getReceivedTime());
-//        String note = returnScheduleDTO.getNote();
-//
-//        schedule.setServiceId(returnScheduleDTO.getServiceId());
-//        schedule.setServiceTypeId(returnScheduleDTO.getTypeId());
-//        schedule.setQuantityRetrieve(1);
-//        schedule.setStartDate(pickupDateTime);
-//        schedule.setEndDate(receivedDateTime);
-//        schedule.setNote(note == null ? "" : note.trim());
-//        schedule.setCycle(returnScheduleDTO.getCycle());
-//        schedule.setStatus(ScheduleStatus.PROCESSING);
-//        schedule.setUserUsageId(returnScheduleDTO.getUserUsageId());
-//
-//        return schedule;
-//    }
-//
-//    public Schedule mapToEntity(DeliveryScheduleDTO deliveryScheduleDTO) {
-//        Schedule schedule = new Schedule();
-//        LocalDateTime startDate = deliveryScheduleDTO.getDate().atTime(deliveryScheduleDTO.getTime());
-//        String note = deliveryScheduleDTO.getNote();
-//
-//        schedule.setServiceId(deliveryScheduleDTO.getServiceId());
-//        schedule.setServiceTypeId(deliveryScheduleDTO.getTypeId());
-//        schedule.setQuantityRetrieve(deliveryScheduleDTO.getQuantity());
-//        schedule.setStartDate(startDate);
-//        schedule.setEndDate(startDate.plusHours(1)); // Default endTime is add 1hr in startDate
-//        schedule.setNote(note == null ? "" : note.trim());
-//        schedule.setCycle(deliveryScheduleDTO.getCycle());
-//        schedule.setStatus(ScheduleStatus.PROCESSING);
-//        schedule.setUserUsageId(deliveryScheduleDTO.getUserUsageId());
-//
-//        return schedule;
-//    }
 
     public EventRes mapToEventRes(Schedule schedule, Service service) {
         EventRes event = new EventRes();
