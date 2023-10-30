8bf27ea4736f7cad1f177175ce2a4777fd387248
hdang09 <dangtranhai628@gmail.com>
2023-10-30 11:04:29 +0700

    REMOVE - Remove PaymentMethod enum value

--- src/main/java/housemate/constants/PaymentMethod.java
+++ src/main/java/housemate/constants/PaymentMethod.java
@@ -1,13 +1,6 @@
 package housemate.constants;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public enum PaymentMethod {
-    VNPAY("vnpay"),
-    MOMO("momo");
-
-    private final String value;
+    VNPAY,
+    MOMO
 }
