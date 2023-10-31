ece6219ce3e2b2588942b1f8dfd5734e36e8d8bb
ThanhF <trthanh179@gmail.com>
2023-10-31 16:09:07 +0700

    ADD - Add demo controller websocket

--- src/main/java/housemate/controllers/WebSocketController.java
+++ src/main/java/housemate/controllers/WebSocketController.java
@@ -4,10 +4,13 @@
  */
 package housemate.controllers;
 
-import housemate.websocketMessages.Message;
-import housemate.websocketMessages.Notification;
+import housemate.models.WebSocketDTO;
+import housemate.services.WebSocketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,21 +20,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebSocketController {
 
+    @Autowired
+    private WebSocketService webSocketService;
+
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
-    public Message send(Message message) {
+    public String send(String message) {
         return message;
     }
 
-    @MessageMapping("/a")
-    @SendTo("/topic/newTask")
-    public Message newTask(Message message) {
-        return message;
+    @PostMapping("/ws/addNewTask")
+    public void sendNotificationForEveryone(@RequestBody WebSocketDTO webSocketDTO) {
+        webSocketService.sendNotificationToEveryone(webSocketDTO);
     }
 
-    @MessageMapping("/notification")
-    @SendTo("/topic/messages")
-    public Notification userNotification(Notification notification) {
-        return notification;
+    @PostMapping("/ws/customer")
+    public void sendNotificationToUser(@RequestBody WebSocketDTO webSocketDTO) {
+        webSocketService.sendNotificationToUser("usertest", webSocketDTO);
     }
+
+    @PostMapping("/ws/send")
+    public void sendNotification(@RequestBody WebSocketDTO webSocketDTO) {
+        webSocketService.sendNotification(webSocketDTO);
+    }
+
 }
