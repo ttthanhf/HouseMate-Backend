b85cd0c6e02c1513bdcb87e9e4e01e49668a915c
hdang09 <dangtranhai628@gmail.com>
2023-11-01 14:41:13 +0700

    ADD - Add validation when create schedule

--- src/main/java/housemate/mappers/ScheduleMapper.java
+++ src/main/java/housemate/mappers/ScheduleMapper.java
@@ -7,23 +7,17 @@ import housemate.models.ScheduleDTO;
 import housemate.responses.EventRes;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-
 @Component
 public class ScheduleMapper {
     public Schedule mapToEntity(ScheduleDTO scheduleDTO) {
         Schedule schedule = new Schedule();
         String note = scheduleDTO.getNote();
-        int quantity = scheduleDTO.getQuantityRetrieve();
-        LocalDateTime startDate = scheduleDTO.getStartDate();
-        LocalDateTime endDate = scheduleDTO.getEndDate();
 
         schedule.setServiceId(scheduleDTO.getServiceId());
         schedule.setServiceTypeId(scheduleDTO.getTypeId());
-        schedule.setQuantityRetrieve(quantity == 0 ? (int) Duration.between(startDate, endDate).toHours() : quantity);
-        schedule.setStartDate(startDate);
-        schedule.setEndDate(endDate);
+        schedule.setQuantityRetrieve(scheduleDTO.getQuantityRetrieve());
+        schedule.setStartDate(scheduleDTO.getStartDate());
+        schedule.setEndDate(scheduleDTO.getEndDate());
         schedule.setNote(note == null ? "" : note.trim());
         schedule.setCycle(scheduleDTO.getCycle());
         schedule.setStatus(ScheduleStatus.PROCESSING);
@@ -34,12 +28,11 @@ public class ScheduleMapper {
 
     public EventRes mapToEventRes(Schedule schedule, Service service) {
         EventRes event = new EventRes();
-        boolean isBeforeCurrentDate = schedule.getEndDate().isBefore(LocalDateTime.now());
 
         event.setTitle(service.getTitleName());
         event.setStart(schedule.getStartDate());
         event.setEnd(schedule.getEndDate());
-        event.setStatus(isBeforeCurrentDate ? ScheduleStatus.CANCEL : schedule.getStatus());
+        event.setStatus(schedule.getStatus());
 
         return event;
     }
