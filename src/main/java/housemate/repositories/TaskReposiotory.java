058fdd9ce7edbe1f5cf2067755571ba0721ce2ed
Hoang Anh Nguyen <nguyenhoanganhgoahead@gmai.com>
2023-11-02 15:51:49 +0700

    commit 3

--- src/main/java/housemate/repositories/TaskReposiotory.java
+++ src/main/java/housemate/repositories/TaskReposiotory.java
@@ -5,10 +5,8 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import housemate.constants.Enum;
 import housemate.constants.Enum.TaskStatus;
 import housemate.entities.Task;
 
@@ -24,6 +22,18 @@ public interface TaskReposiotory extends JpaRepository<Task, Integer> {
 	
 	Page<Task> findAllByTaskStatus(TaskStatus taskStatus, Pageable pagableTaskList);
 	
-	Page<Task> findAllByTaskStatusAndStaffId(TaskStatus taskStatus, Pageable pagableTaskList);
+	@Query("SELECT t FROM Task t WHERE t.staffId = ?1 "
+	     + "AND (?2 IS NULL OR t.taskStatus = ?2) "
+		 + "AND (?2 IS NULL OR t.taskStatus = ?2) "
+	     )
+	Page<Task> findAllByTaskStatusAndStaffId(int staffId , TaskStatus taskStatus, Pageable pagableTaskList);
+	
+	@Modifying
+	@Query("UPDATE Task t SET t.taskStatus = 'INCOMING', t.schedule.status = 'INCOMING' "
+			+ "WHERE t.staffId IS NOT NULL "
+			+ "AND t.schedule.status IN (PENDING)"
+			+ "AND (FUNCTION('TIMESTAMPDIFF', HOUR, CURRENT_TIMESTAMP(), t.schedule.startDate) BETWEEN 0 AND 6) "
+			+ "AND (FUNCTION('MOD', FUNCTION('TIMESTAMPDIFF', MINUTE, CURRENT_TIMESTAMP(), t.schedule.startDate), 60) BETWEEN 0 AND 60) ")
+	void updateTaskStatusIntoIncoming();
 
 }
