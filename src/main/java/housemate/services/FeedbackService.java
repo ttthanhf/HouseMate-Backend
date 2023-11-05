23994ace02f9d7ff8324f55f70738f72a9c1015a
Hoang Anh Nguyen <nguyenhoanganhgoahead@gmai.com>
2023-11-05 17:18:27 +0700

    FORMAT - reformat of feebackService.class

--- src/main/java/housemate/services/FeedbackService.java
+++ src/main/java/housemate/services/FeedbackService.java
@@ -13,9 +13,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import org.springframework.util.Assert;
-
-import housemate.constants.ImageType;
 import housemate.constants.Role;
 import housemate.entities.ServiceFeedback;
 import housemate.entities.UserAccount;
@@ -47,13 +46,15 @@ public class FeedbackService {
 	ImageRepository imgRepo;
 
 	ModelMapper mapper = new ModelMapper();
+	
+	ZoneId datetimeZone = ZoneId.of("Asia/Ho_Chi_Minh");
 
 	public ResponseEntity<?> getRatingOverviewByService(int serviceId) {
 
 		List<ServiceFeedback> serviceFeedbList = feedBackRepo.findAllByServiceId(serviceId);
 		
 		if (serviceFeedbList.isEmpty())
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No feedback for this service");
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Không tìm thấy đánh giá nào cho dịch vụ này");
 
 		FeedbackViewDTO serviceFeedback = new FeedbackViewDTO();
 
@@ -77,7 +78,7 @@ public class FeedbackService {
 		List<ServiceFeedback> serviceFeedbList = feedBackRepo.findAllByServiceId(serviceId);
 
 		if (serviceFeedbList.isEmpty())
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No feedback for this service");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Không tìm thấy nhận xét nào cho dịch vụ này !");
 
 		FeedbackViewDTO serviceFeedback = new FeedbackViewDTO();
 
@@ -85,11 +86,10 @@ public class FeedbackService {
 		for (ServiceFeedback feeback : serviceFeedbList) {
 			FeedbackViewDetailDTO feedbackViewDetail = mapper.map(feeback, FeedbackViewDetailDTO.class);
 			UserAccount customer = userRepo.findByUserId(feeback.getCustomerId());
-			feedbackViewDetail.setCustomerName(customer == null ? "Anonymous" : customer.getFullName());
-			feedbackViewDetail.setAvatar(imgRepo.findAllByEntityIdAndImageType(customer.getUserId(), ImageType.AVATAR).orElse(Collections.EMPTY_LIST));
+			feedbackViewDetail.setCustomerName(customer == null ? "Vô danh" : customer.getFullName());
+			feedbackViewDetail.setAvatar(customer.getAvatar());
 			feebackDetailList.add(feedbackViewDetail);
 		}
-
 		serviceFeedback.setServiceID(serviceId);
 		serviceFeedback.setFeedbackList(feebackDetailList);
 
@@ -97,18 +97,16 @@ public class FeedbackService {
 	}
 	
 	public ResponseEntity<?> findTopFeedback(int ratingLevel) {
-
 		List<ServiceFeedback> serviceFeedbList = feedBackRepo.findTopFeedback(ratingLevel);
-
 		if (serviceFeedbList.isEmpty())
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Danh sách cho top đánh giá hiện đang rỗng !");
 
 		List<FeedbackViewDetailDTO> feebackDetailList = new ArrayList<>();
 		for (ServiceFeedback feeback : serviceFeedbList) {
 			FeedbackViewDetailDTO feedbackViewDetail = mapper.map(feeback, FeedbackViewDetailDTO.class);
 			UserAccount customer = userRepo.findByUserId(feeback.getCustomerId());
-			feedbackViewDetail.setCustomerName(customer == null ? "Anonymous" : customer.getFullName());
-			feedbackViewDetail.setAvatar(imgRepo.findAllByEntityIdAndImageType(customer.getUserId(), ImageType.AVATAR).orElse(Collections.EMPTY_LIST));
+			feedbackViewDetail.setCustomerName(customer == null ? "Vô danh" : customer.getFullName());
+			feedbackViewDetail.setAvatar(customer.getAvatar());
 			feebackDetailList.add(feedbackViewDetail);
 		}
 
@@ -116,12 +114,10 @@ public class FeedbackService {
 	}
 
 	public ResponseEntity<?> filterServiceFeedbackByRating(int serviceId, int ratingLevel) {
-
 		List<ServiceFeedback> serviceFeedbList = feedBackRepo.findAllByRating(serviceId, ratingLevel);
-		
 		if (serviceFeedbList.isEmpty())
-			return ResponseEntity.status(HttpStatus.NOT_FOUND)
-					.body("No feedback for this service with rating " + ratingLevel + " can be found !");
+			return ResponseEntity.status(HttpStatus.NO_CONTENT)
+					.body("Không tìm thấy đánh giá nào có rating " + ratingLevel + " !");
 
 		FeedbackViewDTO serviceFeedback = new FeedbackViewDTO();
 
@@ -129,11 +125,10 @@ public class FeedbackService {
 		for (ServiceFeedback feeback : serviceFeedbList) {
 			FeedbackViewDetailDTO feedbackViewDetail = mapper.map(feeback, FeedbackViewDetailDTO.class);
 			UserAccount customer = userRepo.findByUserId(feeback.getCustomerId());
-			feedbackViewDetail.setCustomerName(customer == null ? "Anonymous" : customer.getFullName());
-			feedbackViewDetail.setAvatar(imgRepo.findAllByEntityIdAndImageType(customer.getUserId(), ImageType.AVATAR).orElse(Collections.EMPTY_LIST));
+			feedbackViewDetail.setCustomerName(customer == null ? "Vô danh" : customer.getFullName());
+			feedbackViewDetail.setAvatar(customer.getAvatar());
 			feebackDetailList.add(feedbackViewDetail);
 		}
-
 		serviceFeedback.setServiceID(serviceId);
 		serviceFeedback.setFeedbackList(feebackDetailList);
 
@@ -142,92 +137,89 @@ public class FeedbackService {
 
 	public ResponseEntity<?> findAll() {
 		List<ServiceFeedback> serviceFeedbList = feedBackRepo.findAll();
-		
 		if(serviceFeedbList.isEmpty())
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Empty list now");
-		
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Không có đánh giá nào để hiển thị ! Hãy tạo đánh giá !");
+	
 		return ResponseEntity.ok(serviceFeedbList);
 	}
 
 	public ResponseEntity<?> getOne(int serviceFeedbackId) {
 		ServiceFeedback feedback = feedBackRepo.findById(serviceFeedbackId).orElse(null);
-		
 		if(feedback == null)
-			return ResponseEntity.badRequest().body("This feedback does not exist");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Thông tin về đánh giá này không tồn tại !");
 		
 		FeedbackViewDetailDTO feedbackViewDetail = mapper.map(feedback, FeedbackViewDetailDTO.class);
 		UserAccount customer = userRepo.findByUserId(feedback.getCustomerId());
 		feedbackViewDetail.setCustomerName(customer == null ? "Anonymous" : customer.getFullName());
-		feedbackViewDetail.setAvatar(imgRepo.findAllByEntityIdAndImageType(customer.getUserId(), ImageType.AVATAR).orElse(Collections.EMPTY_LIST));
+		feedbackViewDetail.setAvatar(customer.getAvatar());
 		
 		return ResponseEntity.ok(feedbackViewDetail);
 	}
 
 	@Transactional
 	public ResponseEntity<?> createNewFeedback(HttpServletRequest request, FeedbackNewDTO newFeedback) {
-
-		int userId = authorizationUtil.getUserIdFromAuthorizationHeader(request);
+		ServiceFeedback feedbackToSave = null;
+		try {
+			//TODO: Constraint for comboId taskId, customerId, ServiceId In Here
+			//TODO: Allow to create when task status is DONE
+			int userId = authorizationUtil.getUserIdFromAuthorizationHeader(request);
+			if(feedBackRepo.findByTaskId(newFeedback.getTaskId()) != null)
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Đánh giá này đã được tạo ! Hãy chuyển sang cập nhật nó nếu bạn muốn chỉnh sửa đánh giá nhé  !");
+			
+			feedbackToSave = mapper.map(newFeedback, ServiceFeedback.class);
+			feedbackToSave.setCreatedAt(LocalDateTime.now(datetimeZone));
+			feedbackToSave.setCustomerId(userId);
+			feedbackToSave = feedBackRepo.save(feedbackToSave);
+			Assert.notNull(feedbackToSave, "Có lỗi đã xảy ra ! Tạo đánh giá thất bại ! Hãy thử tạo lại !");
+			servRepo.updateAvgRating(feedbackToSave.getServiceId());
+		}catch(Exception e) {
+			TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Có lỗi đã xảy ra ! Tạo đánh giá thất bại ! Hãy thử tạo lại !");
+		}
 		
-		//TODO: Constraint for comboId taskId, customerId, ServiceId In Here
-		//One task has only one feedback
-		if(feedBackRepo.findByTaskId(newFeedback.getTaskId()) != null)
-			return ResponseEntity.badRequest().body("This feedback has existed ! Only allow to update !");
-		
-		ServiceFeedback feedback = mapper.map(newFeedback, ServiceFeedback.class);
-		feedback.setCreatedAt(LocalDateTime.now(ZoneId.of("Asia/Ho_Chi_Minh")));
-		feedback.setCustomerId(userId);
-		ServiceFeedback addedFeedback = feedBackRepo.save(feedback);
-
-		Assert.notNull(addedFeedback, "Saved failed !");
-		
-		servRepo.updateAvgRating(feedback.getServiceId());
-		
-		return this.getOne(feedback.getServiceFeedbackId());
+		return this.getOne(feedbackToSave.getServiceFeedbackId());
 	}
 
 	@Transactional
 	public ResponseEntity<?> updateFeedback(HttpServletRequest request, FeedbackNewDTO newFeedback, int serviceFeedbackId) {
-
-		int currentUserId = authorizationUtil.getUserIdFromAuthorizationHeader(request);
+		ServiceFeedback feedbackToUpdate = null;
+		try {
+			int currentUserId = authorizationUtil.getUserIdFromAuthorizationHeader(request);
+			//TODO: Constraint for comboId taskId, customerId, ServiceId In Here
+			//TODO: Allow to create when task status is DONE
+			//Only the author of specific feedback is allowed to update include Admin not allow too
+			ServiceFeedback oldFeedback = feedBackRepo.findFeedback(serviceFeedbackId, currentUserId, newFeedback.getTaskId(), newFeedback.getServiceId());
+			if(oldFeedback == null)
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Không tồn tại đánh giá này !");
+			
+			oldFeedback.setContent(newFeedback.getContent());
+			oldFeedback.setRating(newFeedback.getRating());
+			oldFeedback.setCreatedAt(LocalDateTime.now(datetimeZone));
+			feedbackToUpdate = feedBackRepo.save(oldFeedback);
+			Assert.notNull(feedbackToUpdate, "Có lỗi đã xảy ra ! Cập nhật đánh giá thất bại !");			
+			servRepo.updateAvgRating(oldFeedback.getServiceId());
+		}catch(Exception e) {
+			TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Có lỗi đã xảy ra ! Cập nhật đánh giá thất bại !");
+		}
 		
-		//TODO: Constraint for comboId taskId, customerId, ServiceId In Here
-		
-		//Only the author of specific feedback is allowed to update include Admin not allow too
-		ServiceFeedback oldFeedback = feedBackRepo.findFeedback(serviceFeedbackId, currentUserId, newFeedback.getTaskId(), newFeedback.getServiceId());
-
-		if(oldFeedback == null)
-			return ResponseEntity.badRequest().body("This feedback does not exist for you to update !");
-
-		oldFeedback.setContent(newFeedback.getContent());
-		oldFeedback.setRating(newFeedback.getRating());
-		oldFeedback.setCreatedAt(LocalDateTime.now(ZoneId.of("Asia/Ho_Chi_Minh")));
-		ServiceFeedback updatedFeedback = feedBackRepo.save(oldFeedback);
-
-		Assert.notNull(updatedFeedback, "Update Failed !");
-		
-		servRepo.updateAvgRating(oldFeedback.getServiceId());
-		
-		return this.getOne(oldFeedback.getServiceFeedbackId());
+		return this.getOne(feedbackToUpdate.getServiceFeedbackId());
 	}
 	
 	@Transactional
 	public ResponseEntity<?> removeFeedback(HttpServletRequest request, int serviceFeedbackId) {
-
 		int currentUserId = authorizationUtil.getUserIdFromAuthorizationHeader(request);
-
+		Role currentUserRole = Role.valueOf(authorizationUtil.getRoleFromAuthorizationHeader(request));
 		ServiceFeedback feedback = feedBackRepo.findById(serviceFeedbackId).orElse(null);
-
 		if(feedback == null) 
-			return ResponseEntity.badRequest().body("This feedback does not exist for removing !");
-
-		if (!(currentUserId == feedback.getCustomerId())) // isAuthor = false
-			if (!authorizationUtil.getRoleFromAuthorizationHeader(request).equals(Role.ADMIN.toString())) // isAdmin = false
-				return ResponseEntity.badRequest().body("You are not allowed to delete this feedback !");
-
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Không tồn tại đánh giá này !");
+		if (!(currentUserId == feedback.getCustomerId() || currentUserRole.equals(Role.ADMIN))) 
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bạn không có quyền xóa đánh giá này !");
 		feedBackRepo.deleteById(serviceFeedbackId);
 		servRepo.updateAvgRating(serviceFeedbackId);
-
-		return ResponseEntity.ok("Remove Successfully");
+		
+		return ResponseEntity.ok("Xóa thành công");
 	}
-
 }
