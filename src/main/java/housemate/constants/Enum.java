d931b50b33c3ac6d321783fac5aad555ede26242
Hoang Anh Nguyen <nguyenhoanganhgoahead@gmai.com>
2023-11-07 01:00:44 +0700

    FIX - error get service config value

--- src/main/java/housemate/constants/Enum.java
+++ src/main/java/housemate/constants/Enum.java
@@ -1,7 +1,15 @@
 package housemate.constants;
 
+
+import java.util.EnumSet;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import housemate.repositories.ServiceConfigRepository;
 import housemate.services.ServiceConfigService;
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -42,12 +50,9 @@ public class Enum {
 	
 	@Getter
 	public enum ServiceConfiguration {
-	    SERVICE_GROUPS(true),
-	    SERVICE_UNITS(true),
-	    OFFICE_HOURS_START(false),
-	    OFFICE_HOURS_END(false),
-	    FIND_STAFF_HOURS(false),
-	    MINIMUM_RETURN_HOURS(false),
+	    SERVICE_GROUPS(true), SERVICE_UNITS(true),
+	    OFFICE_HOURS_START(false), OFFICE_HOURS_END(false),
+	    FIND_STAFF_HOURS(false), MINIMUM_RETURN_HOURS(false),
 	    DURATION_HOURS_CUSTOMER_SHOULD_NOT_CANCEL_TASK(false),
 	    DURATION_HOURS_STAFF_SHOULD_NOT_CANCEL_TASK(false),
 	    DURATION_HOURS_ALLOW_BAD_STAFF_PROFICENT_SCORE_APPLY(false),
@@ -56,20 +61,40 @@ public class Enum {
 	    MINUS_POINTS_FOR_STAFF_CANCEL_TASK(false),
 	    BAD_STAFF_PROFICIENT_SCORE(false);
 
+	    private ServiceConfigService configService;
 	    private boolean isMultiValue;
-	    
+
 	    private ServiceConfiguration(boolean isMultiValue) {
 		this.isMultiValue = isMultiValue;
 	    }
+	    public void setServiceConfig(ServiceConfigService serviceConfig) {
+		this.configService = serviceConfig;
+	    }
+	    
+	    @Component
+	    public static class ServiceConfigInjector {
+		@Autowired
+		ServiceConfigService serviceConfig;
 
-	    @Autowired
-	    ServiceConfigService configService;
-
+		@PostConstruct
+		public void postConstruct() {
+		    for (ServiceConfiguration config : EnumSet.allOf(ServiceConfiguration.class)) {
+			config.setServiceConfig(this.serviceConfig);
+		    }
+		}
+	    }
+	    
 	    public Integer getNum() {
-		String configValue = configService.getConfigValuesOfConfigTypeName(this.valueOf(this.name())).stream()
-			.findFirst().orElseThrow(IllegalArgumentException::new);
+		String configValue = null;
+		try {
+		    configValue = configService.getConfigValuesOfConfigTypeName(this.valueOf(this.name())).stream().findFirst()
+				.orElseThrow(IllegalArgumentException::new);
+		}catch (Exception e) {
+		    e.printStackTrace();
+		}
 		return configValue == null ? null : Integer.parseInt(configValue);
 	    }
+
 	}
 	
 	public enum AccountStatus {
