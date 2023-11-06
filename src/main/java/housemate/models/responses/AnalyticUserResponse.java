46c2a1363abcd04c376a934b07fbc6205fe3cc23
ThanhF <trthanh179@gmail.com>
2023-11-06 21:57:41 +0700

    ADD - Add feature analytic

---
+++ src/main/java/housemate/models/responses/AnalyticUserResponse.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package housemate.models.responses;
+
+import lombok.Data;
+
+/**
+ *
+ * @author ThanhF
+ */
+@Data
+public class AnalyticUserResponse {
+
+    private String year;
+    private String month;
+    private String day;
+    private String sessions;
+    private String newUsers;
+    private String activeUsers;
+
+}
