4182f94c40c62383a2e3cff8f00b81d669881f04
hdang09 <dangtranhai628@gmail.com>
2023-11-10 14:57:27 +0700

    ADD - Create staff account

--- src/main/java/housemate/mappers/AccountMapper.java
+++ src/main/java/housemate/mappers/AccountMapper.java
@@ -6,9 +6,12 @@ package housemate.mappers;
 
 import housemate.constants.Role;
 import housemate.entities.UserAccount;
+import housemate.models.CreateAccountDTO;
 import housemate.models.RegisterAccountDTO;
 import housemate.models.UpdateAccountDTO;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 /**
  * @author Admin
@@ -18,6 +21,8 @@ import org.springframework.stereotype.Component;
 public class AccountMapper {
 
     final String DEFAULT_AVATAR = "https://scontent.fsgn2-9.fna.fbcdn.net/v/t1.15752-9/384469032_6609223889131065_8293022876449520388_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=ae9488&_nc_ohc=gjDXwSBmi3YAX-hNO9i&_nc_ht=scontent.fsgn2-9.fna&oh=03_AdTYPieo_8M2sWscLr-rykTpN-IAaBS8JRWarwlkJQpKdA&oe=6540E586";
+    final int MAX_PROFICIENCY_SCORE = 100;
+    final int MAX_AVERAGE_RATING = 5;
 
     public UserAccount mapToEntity(RegisterAccountDTO registerAccountDTO) {
         UserAccount userAccount = new UserAccount();
@@ -29,9 +34,29 @@ public class AccountMapper {
         userAccount.setEmailValidationStatus(false);
         userAccount.setPasswordHash(registerAccountDTO.getPassword());
         userAccount.setAvatar(DEFAULT_AVATAR);
+        userAccount.setCreatedAt(LocalDateTime.now());
 
-	        return userAccount;
+	    return userAccount;
 	    }
+
+    public UserAccount mapToEntity(CreateAccountDTO createAccountDTO) {
+        UserAccount userAccount = new UserAccount();
+
+        userAccount.setFullName(createAccountDTO.getFullName());
+        userAccount.setDateOfBirth(createAccountDTO.getDateOfBirth());
+        userAccount.setSex(createAccountDTO.getSex());
+        userAccount.setPhoneNumber(createAccountDTO.getPhone());
+        userAccount.setIdentityCard(createAccountDTO.getIdentityCard());
+        userAccount.setEmailAddress(createAccountDTO.getEmail());
+        userAccount.setAddress(createAccountDTO.getAddress());
+        userAccount.setRole(Role.STAFF);
+        userAccount.setProficiencyScore(MAX_PROFICIENCY_SCORE);
+        userAccount.setAvgRating(MAX_AVERAGE_RATING);
+
+        userAccount.setEmailValidationStatus(true);
+
+        return userAccount;
+    }
 
 	    public UserAccount updateAccount(UserAccount currentAccount, UpdateAccountDTO updatedAccount) {
 	        currentAccount.setFullName(updatedAccount.getFullName());
